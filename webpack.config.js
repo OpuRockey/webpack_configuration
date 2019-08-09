d2b13e701bc0943d40fbefe17532a7b6bee6e95a
opurockey <opu.rockey@gmail.com>
2019-08-09 16:15:29 +0600

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const HtmlWebPackPlugin = require ('html-webpack-plugin');
 const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
+const webpack = require('webpack');
+const path = require('path');
 
 
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
     module: {
         rules: [
             {
@@ -42,6 +49,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new webpack.ProgressPlugin(),
         new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "./index.html"
